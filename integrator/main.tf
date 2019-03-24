f2bf0590549a48bff52593d5e2818fee1a48ed4c
grupomult <leogsilva@gmail.com>
2019-03-24 15:46:05 -0300

    Adding source attribute

--- integrator/main.tf
+++ integrator/main.tf
@@ -1,5 +1,6 @@
 
 module "s3-bucket" {
+  source        = "git::https://github.com/felipefrizzo/terraform-aws-s3-bucket.git?ref=master"
   bucket_name   = "${var.bucket_name}"
   force_destroy = true
   public        = false
